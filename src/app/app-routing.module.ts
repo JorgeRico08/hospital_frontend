e0d8cf66542040279f92e9a209f3d5928cb198f7
GeorGe-0812 <ricolinoarvizu04@gmail.com>
2022-10-31 17:31:49 -0600

    Actualizacion P1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import { HomeComponent } from './components/pages/home/home.component';
+import { EventoComponent } from './components/evento/evento.component';
+import { ExpedienteComponent } from './components/expediente/expediente.component';
+
+const routes: Routes = [
+
+  { path: '', component: HomeComponent },
+  { path: 'expediente', component: ExpedienteComponent},
+  { path: 'evento', component: EventoComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExpedienteComponent } from './components/expediente/expediente.component';
+import { EventoComponent } from './components/evento/evento.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NavBarComponent } from './components/pages/nav-bar/nav-bar.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpedienteComponent
+    ExpedienteComponent,
+    EventoComponent,
+    HomeComponent,
+    NavbarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
